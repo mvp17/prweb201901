afe2045ff261d1ff67c68ace409d2bf9436b3350
Marc Visa <mvisapascual@gmail.com>
2019-02-21 11:39:27 +0100

    List of activities with title and link added

--- ClientUdlHTTP.py
+++ ClientUdlHTTP.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python
-#vim fileencoding = utf-8
+# vim encoding = utf-8
 
 from urllib.request import urlopen
 import bs4
 
+
 class ClientWeb(object):
     """Client web per la web de la EPS"""
+
     def __init__(self):
         super(ClientWeb, self).__init__()
 
-    def descarregar_html():
+    def descarregar_html(self):
         f = urlopen("http://www.eps.udl.cat/ca/")
         html = f.read()
         f.close()
         return html
 
     def buscar_activitats(self, html):
-        arbre = bs4.BeautifulSoup(html, features = "lxml")
-        activitats = arbre.find_all("div","featured-links-item")
-        return activitats
-
+        arbre = bs4.BeautifulSoup(html, features="lxml")  # TODO fix features
+        activitats = arbre.find_all("div", "featured-links-item")
+        activity_list = []
+        for activity in activitats:
+            title = activity.find("span", "flink-title")
+            link = activity.find("a")
+            activity_list.append((title.text, link["href"]))
+        return activity_list
 
     def run(self):
-        #descarregar-me html
         html = self.descarregar_html()
-        #buscar activitats
         activitats = self.buscar_activitats(html)
-        #imprimir resultat
         print(activitats)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     c = ClientWeb()
     c.run()
