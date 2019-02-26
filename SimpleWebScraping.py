5a37d9450ed1f06ae944a870737d79495d8cb3fd
Marc Visa <mvisapascual@gmail.com>
2019-02-26 16:34:02 +0100

    Optional Activity finished (SimpleWebScraping.py). Get Html with json in WeatherHTTP.py

---
+++ SimpleWebScraping.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set file encoding = utf-8
+
+from urllib.request import urlopen
+import bs4
+
+
+class WebScraping(object):
+
+    def __init__(self):
+        super(WebScraping, self).__init__()
+
+    @staticmethod
+    def get_html():
+        url = urlopen("https://www.banggood.com/Flashdeals.html")
+        data = url.read()
+        url.close()
+        return data
+
+    @staticmethod
+    def scraping_process(html):
+        tree = bs4.BeautifulSoup(html, "lxml")
+        text = tree.find_all("div", "scroll_box")
+        elements = []
+        for element in text:
+            offer = element.find("span", "price")
+            regular = element.find("span", "price_old")
+            title = element.find("span", "title")
+            elements.append((title.text, (offer.text, regular.text)))
+        return elements
+
+    def run(self):
+        html = self.get_html()
+        data = self.scraping_process(html)
+        print(data)
+
+
+if __name__ == "__main__":
+    webScraping = WebScraping()
+    webScraping.run()
--- WeatherHTTP.py
+++ WeatherHTTP.py
@@ -3,7 +3,9 @@
 # vim: set file encoding = utf-8
 
 from urllib.request import urlopen
-import bs4
+# import bs4
+# import xmltodict
+import json
 
 
 class ClientWeb(object):
@@ -12,21 +14,14 @@ class ClientWeb(object):
     def __init__(self):
         super(ClientWeb, self).__init__()
 
-    def do_request(self):
+    @staticmethod
+    def do_request():
         # https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&appid=81e8695c285ce7e2fba13e0c7d7d811d
         f = urlopen("https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&appid"
-                    "=81e8695c285ce7e2fba13e0c7d7d811d")
+                    "=81e8695c285ce7e2fba13e0c7d7d811d&mode=json&lang=ca")
         data = f.read()
         f.close()
         return data
-
-    def process_weather(self, html):
-        print(html)
-        arbre = bs4.BeautifulSoup(html, features="lxml")  # TODO: fix features
-        temperature = arbre.find("temperature")
-        weather = arbre.find("weather")
-        print(temperature["value"] + "and" + weather["value"])
-        return " "
 
     def run(self):
         # download html
@@ -35,6 +30,25 @@ class ClientWeb(object):
         data = self.process_weather(data)
         # print data
         print(data)
+
+    @staticmethod
+    def process_weather(html):
+        # dic = xmltodict.parse(html)
+        dic1 = json.loads(html)
+        temp = dic1['list'][0]['main']['temp']
+        weather = dic1['list'][0]['weather'][0]['description']
+        return str(temp)+" and "+str(weather)
+
+
+"""
+            def process_weather(self, html):
+                print(html)
+                arbre = bs4.BeautifulSoup(html, features="lxml")  # TODO: fix features
+                temperature = arbre.find("temperature")
+                weather = arbre.find("weather")
+                print(temperature["value"] + "and" + weather["value"])
+                return " "
+"""
 
 
 if __name__ == "__main__":
