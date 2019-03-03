c8dd75a6a04759448d5575c8dd2473492eb0664f
Marc Visa <mvisapascual@gmail.com>
2019-03-03 17:57:10 +0100

    Scrape the title and the prices separately and join them as tuples like (title, current price, old price).

--- SimpleWebScraping.py
+++ SimpleWebScraping.py
@@ -12,7 +12,7 @@ class WebScraping(object):
         super(WebScraping, self).__init__()
 
     @staticmethod
-    def get_html():
+    def getHtml():
         url = urlopen("https://www.banggood.com/Flashdeals.html")
         data = url.read()
         url.close()
@@ -22,35 +22,41 @@ class WebScraping(object):
     def getTitles(tree):
         treeTitles = tree.find_all("span", "title")
         titles = []
-
         for title in treeTitles:
             titles.append(title.find("a").text)
-
         return titles
 
     @staticmethod
     def getPrices(tree):
         treePrices = tree.find_all("div", "priceitem")
         prices = []
-
         for price in treePrices:
             prices.append((price.find("span", "price").text, price.find("span", "price_old").text))
-
         return prices
 
     @staticmethod
-    def printData(titles, prices):
-        for i in range(len(titles)):
-            print("Title: ", titles[i], " ", "Current Price: ", prices[i][0], " ", "Old Price: ", prices[i][1])
-            print("\n")
+    def printData(tuples):
+        print("\n")
+        for i in range(len(tuples)):
+            print("Title: ", tuples[i][0], " ", "Current Price: ", tuples[i][1], " ", "Old Price: ", tuples[i][2])
 
-    def scraping_process(self, html):
+    @staticmethod
+    def getTuples(titles, prices):
+        tuples = []
+        for i in range(len(titles)):
+            tuples.append((titles[i], prices[i][0], prices[i][1]))
+        return tuples
+
+    def scrapingProcess(self, html):
         tree = bs4.BeautifulSoup(html, "lxml")
-        self.printData(self.getTitles(tree), self.getPrices(tree))
+        titles = self.getTitles(tree)
+        prices = self.getPrices(tree)
+        tuples = self.getTuples(titles, prices)
+        self.printData(tuples)
 
     def run(self):
-        html = self.get_html()
-        self.scraping_process(html)
+        html = self.getHtml()
+        self.scrapingProcess(html)
 
 
 if __name__ == "__main__":
